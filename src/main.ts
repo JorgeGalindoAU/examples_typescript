d48c57c7df00514d1d2246e2e9b48bb57f7e6cb7
Jorge Galindo <jorge.galindo.zetes@gmail.com>
2025-05-28 22:31:15 +0200

    Added new Run configuration and modified main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
-let test: string = 'Hello World!';
+function greet(person: string) {
+    return "Hello, " + person;
+}
+
+const test: string = 'Hello World!';
 
 console.log(`Mi mensaje: ${test}`);
+
+const user: string = "Jane User";
+let message: string = greet(user);
+
+console.log(MessageEvent);
